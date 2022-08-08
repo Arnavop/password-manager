b2b5cefcbd45002a8e3d0d5b3b4a7a74ccbc3b7e
Arnavop <arnavkamramalout@gmail.com>
2022-08-08 13:31:54 +0530

    added method to find password

--- main.py
+++ main.py
@@ -38,10 +38,24 @@ def save():
         is_ok = messagebox.askokcancel(title=website,message=f"These are the details entered: \nEmail :{email} \nPassword : {password} \nPress ok to save:")
         if is_ok:
             with open("data.txt","a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+                data_file.write(f"{website.lower()} | {email} | {password}\n")
                 website_entry.delete(0,END)
                 password_entry.delete(0,END)
 
+def find_passwd():
+    website = website_entry.get()
+    if len(website) ==0:
+        messagebox.showinfo(title="Error",message="Please make sure you havent left any field empty")
+
+    else:
+        with open("data.txt","r") as data_file:
+            data = data_file.readlines()
+            for i in data:
+                if website.lower() in i:
+                    messagebox.showinfo(title="Password found",message=i)
+                    break
+            else:
+                messagebox.showinfo(title="Error",message=f"No entry for {website} found in the file")
 
 
 
@@ -62,8 +76,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3,column=0)
 
 
-website_entry = Entry(width=70)
-website_entry.grid(row=1,column=1,columnspan=2)
+website_entry = Entry(width=55)
+website_entry.grid(row=1,column=1)
 email_entry = Entry(width=70)
 email_entry.grid(row=2,column=1,columnspan=2)
 email_entry.insert(0,"arnavkamramalout@gmail.com")
@@ -74,5 +88,8 @@ generate_password_button = Button(text="Generate Button",command=generate_passwo
 generate_password_button.grid(row = 3,column=2)
 add_button = Button(text="Add",width=60,command=save)
 add_button.grid(row=4,column=1,columnspan=2)
+
+find_password = Button(text="Find Password",command=find_passwd)
+find_password.grid(row=1,column=2)
 
 window.mainloop()
